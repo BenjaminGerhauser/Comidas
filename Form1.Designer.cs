01f2183bbbc1eb05cf3af483881e5573cbc8edd9
Alumnos <Alumnos@PC61>
2023-09-21 10:50:25 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Comidas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboPersona = new System.Windows.Forms.ComboBox();
+            this.grilla = new System.Windows.Forms.DataGridView();
+            this.Comida = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grilla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccione una persona";
+            // 
+            // cboPersona
+            // 
+            this.cboPersona.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPersona.FormattingEnabled = true;
+            this.cboPersona.Location = new System.Drawing.Point(62, 68);
+            this.cboPersona.Name = "cboPersona";
+            this.cboPersona.Size = new System.Drawing.Size(121, 21);
+            this.cboPersona.TabIndex = 1;
+            this.cboPersona.SelectedIndexChanged += new System.EventHandler(this.cboPersona_SelectedIndexChanged);
+            // 
+            // grilla
+            // 
+            this.grilla.AllowUserToAddRows = false;
+            this.grilla.AllowUserToDeleteRows = false;
+            this.grilla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grilla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Comida,
+            this.Nombre});
+            this.grilla.Location = new System.Drawing.Point(62, 113);
+            this.grilla.Name = "grilla";
+            this.grilla.ReadOnly = true;
+            this.grilla.Size = new System.Drawing.Size(522, 224);
+            this.grilla.TabIndex = 2;
+            // 
+            // Comida
+            // 
+            this.Comida.HeaderText = "Comida";
+            this.Comida.Name = "Comida";
+            this.Comida.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 300;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 450);
+            this.Controls.Add(this.grilla);
+            this.Controls.Add(this.cboPersona);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grilla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboPersona;
+        private System.Windows.Forms.DataGridView grilla;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Comida;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comidas
+{
+    public partial class Form1 : Form
+    {
+        Personas oPersona;
+        DataTable tper;
+        Comidas oComida;
+        DataTable tcom;
+        MeGusta oMeGusta;
+        DataTable tGusta;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            oPersona = new Personas();
+            tper = oPersona.getData();
+
+            oComida = new Comidas();
+            tcom = oComida.getData();
+
+            oMeGusta = new MeGusta();
+            tGusta = oMeGusta.getData();
+
+            cboPersona.DisplayMember = "nombre";
+            cboPersona.ValueMember = "dni";
+            cboPersona.DataSource = tper;
+        }
+
+        private void cboPersona_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            grilla.Rows.Clear();
+            foreach (DataRow fGus in tGusta.Rows)
+            {
+                if (fGus["dni"].ToString()== cboPersona.SelectedValue.ToString())
+                {
+                    DataRow fcom = tcom.Rows.Find(fGus["comida"]);
+                    grilla.Rows.Add(fcom["comida"], fcom["nombre"]);
+                }
+            }
+        }
+    }
+}
